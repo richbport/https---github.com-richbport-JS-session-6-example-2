2e9d5e11a637771c658f876aeb4c16a1792aea18
Rich <richbport@yahoo.com>
2025-08-20 14:01:02 -0400

    deleteButton

--- todo.js
+++ todo.js
@@ -2,25 +2,32 @@ let taskInput = document.addEventListener("taskInput");
 let addTaskButton = document.addEventListener("addTask");
 let taskList = document.addEventListener("taskList");
 
+// Loading saved tasks
 let savedTasks = JSON.parse(localStorage.tasks.getItem("tasks")) || [];
 savedTasks.forEach((task) => {
   addTaskToDom(task);
 });
 
+// Add a task
 addTaskButton.addEventListener("click", function() {
   let task = taskInput.value.trim();
   if (task) {
     savedTasks.push(task)
     localStorage.setItem("tasks", JSON.stringify(savedTasks));
     addTaskToDom(task);
-    taskInput.value = ""
+    taskInput.value = ""; // clearing out the input field
   }
 })
 
-
+// Adding a task to the DOM (self explanatory)
 function addTaskToDom(task) {
   let li = document.createElement("li");
   li.textContent = task
 
   // add a delete button
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", function() {
+    taskList.removeChild(li);
+  })
 }
